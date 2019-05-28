3d0ac6908a04a4ddd6b02607a81384903c81baa2
Maaduu <mamadubalde644@yahoo.com>
2019-05-28 06:41:17 +0200

    Add json file

--- creating_folder.py
+++ creating_folder.py
@@ -1,6 +1,6 @@
 # Importing Operating System module
 import os
-
+import json
 
 def creating_folder(folders):
   for key, values in folders.items():
@@ -9,9 +9,17 @@ def creating_folder(folders):
       os.makedirs(folder)
       print('Creating folder {0}'.format(folder))
 
+def creating_json_file(path_json_file, dictionnaire):
+  with open(path_json_file, 'w') as f:
+    json.dump(dictionnaire, f, indent=4)
+
+
 path_folder = r'C:\wamp64\www\Dev\u\Python\in_git\create_a_folder_structure\creating_folder\structure'
 path_folder = path_folder.replace('\\', '/')
 #os.listdir(path_folder)
+
+path_json_file = r'C:\wamp64\www\Dev\u\Python\in_git\create_a_folder_structure\creating_folder\structure\structure.json'
+path_json_file = path_json_file.replace('\\', '/')
 
 structure = {
   'Movies': ['Game of Trhones', 'Get rich or die', 'Dogs of instambul'],
@@ -20,4 +28,5 @@ structure = {
 
 # Create the folders
 creating_folder(structure)
-
+# Create the json file
+creating_json_file(path_json_file, structure)
