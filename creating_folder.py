c840f848460534a408a93a0f62be41b252502c4a
Maaduu <mamadubalde644@yahoo.com>
2019-05-28 06:45:08 +0200

    Add print hierarchy folders

--- creating_folder.py
+++ creating_folder.py
@@ -2,6 +2,18 @@
 import os
 import json
 
+def print_hierarchie(path_json_file):
+      with open(path_json_file, 'r') as f:
+    structure = json.load(f)
+    print('The files already exist')
+    print('Here is the hierarchy of folders')
+    for key, values in structure.items():
+      print('.{0}'.format(key))
+      for value in values:
+        print('...{0}'.format(value))
+      print('='*30)
+
+
 def creating_folder(folders):
   for key, values in folders.items():
     for value in values:
@@ -25,8 +37,12 @@ structure = {
   'Movies': ['Game of Trhones', 'Get rich or die', 'Dogs of instambul'],
   'Music': ['Rock', 'Rap', 'Reggae']
 }
-
-# Create the folders
-creating_folder(structure)
-# Create the json file
-creating_json_file(path_json_file, structure)
+# If the files already exist
+if os.path.isfile(path_json_file):
+  # Print the hierarchy of folders
+  print_hierarchie(path_json_file)
+else: # If the files doesn't exist
+  # Create the folders
+  creating_folder(structure)
+  # Create the json file
+  creating_json_file(path_json_file, structure)
